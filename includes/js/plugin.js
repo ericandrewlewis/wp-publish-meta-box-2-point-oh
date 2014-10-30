dcc1e20e626bf7075b0fd37885045f908ab6abf4
Eric Lewis <eric.andrew.lewis@gmail.com>
2014-10-30 12:57:55 -0700

    Convert dropdown to radio buttons.

--- includes/js/plugin.js
+++ includes/js/plugin.js
@@ -1,5 +1,4 @@
 (function( $, Backbone, _ ) {
-
 	/**
 	 * Ensure clicking buttons inside #submitpost don't trigger the submit event.
 	 *
@@ -19,282 +18,110 @@
 			 */
 			initialize: function() {
 				this.model = new Backbone.Model( window.radPublishMetaBoxData );
-				this.$el = $('.misc-pub-rad-publish-container');
-
-				this.createDropdown();
 				this.initializeDatepicker();
 
-				this.$el.on( 'click', '.publish-action-update', _.bind( this.handleUpdateClick, this ) );
-				this.$el.on( 'click', '.publish-action-publish-privately', _.bind( this.handlePublishPrivatelyClick, this ) );
-				this.$el.on( 'click', '.publish-action-publish-publicly', _.bind( this.handlePublishPubliclyClick, this ) );
-				this.$el.on( 'click', '.publish-action-preview', _.bind( this.handlePreviewClick, this ) );
-				this.$el.on( 'click', '.publish-action-trash', _.bind( this.handleTrashClick, this ) );
-				this.$el.on( 'click', '.publish-action-save-draft', _.bind( this.handleSaveDraftClick, this ) );
-				this.$el.on( 'click', '.publish-action-save-as-pending-review', _.bind( this.handleSaveAsPendingReviewClick, this ) );
-				this.$el.on( 'click', '.publish-action-publish-now', _.bind( this.handlePublishNowClick, this ) );
-				this.$el.on( 'click', '.publish-action-show-schedule-interface', _.bind( this.handleShowScheduleInterfaceClick, this ) );
-				this.$el.on( 'click', '.publish-action-schedule', _.bind( this.handleScheduleClick, this ) );
-				this.$('.hour-dropdown,.ampm-dropdown,.minute-input').on( 'change keyup', _.bind( this.handleTimeInputsChange, this ) );
-			},
-
-			/**
-			 * Create the Bootstrap dropdown and append it to #misc-publishing-actions.
-			 */
-			createDropdown: function() {
-				if ( this.model.get( 'post_status' ) === 'publish' ) {
-					this.publishingOptions = [
-						{
-							title: 'Update',
-							slug: 'update'
-						},
-						{
-							title: 'Publish privately',
-							slug: 'publish-privately'
-						},
-						{
-							title: 'Schedule',
-							slug: 'show-schedule-interface'
-						},
-						{
-							title: 'Trash',
-							slug: 'trash'
-						},
-						{
-							title: 'Send back to pending review',
-							slug: 'save-as-pending-review'
-						},
-						{
-							title: 'Send back to draft',
-							slug: 'save-draft'
-						}
-					];
-				}
-				if ( this.model.get( 'post_status' ) === 'private' ) {
-					this.publishingOptions = [
-						{
-							title: 'Update',
-							slug: 'update'
-						},
-						{
-							title: 'Publish publicly',
-							slug: 'publish-publicly'
-						},
-						{
-							title: 'Schedule',
-							slug: 'show-schedule-interface'
-						},
-						{
-							title: 'Trash',
-							slug: 'trash'
-						},
-						{
-							title: 'Send back to pending peview',
-							slug: 'save-as-pending-review'
-						},
-						{
-							title: 'Send back to draft',
-							slug: 'save-draft'
-						}
-					];
-				}
-				if ( this.model.get( 'post_status' ) === 'future' ) {
-					this.publishingOptions = [
-						{
-							title: 'Update',
-							slug: 'update'
-						},
-						{
-							title: 'Publish now',
-							slug: 'publish-now'
-						},
-						{
-							title: 'Schedule',
-							slug: 'show-schedule-interface'
-						},
-						{
-							title: 'Publish privately',
-							slug: 'publish-privately'
-						},
-						{
-							title: 'Trash',
-							slug: 'trash'
-						},
-						{
-							title: 'Send back to pending review',
-							slug: 'save-as-pending-review'
-						},
-						{
-							title: 'Send back to draft',
-							slug: 'save-draft'
-						}
-					];
-				}
-				if ( _.contains( [ 'auto-draft', 'draft' ], this.model.get( 'post_status' ) ) ) {
-					this.publishingOptions = [
-						{
-							title: 'Publish Now',
-							slug: 'update'
-						},
-						{
-							title: 'Publish privately',
-							slug: 'publish-privately'
-						},
-						{
-							title: 'Schedule',
-							slug: 'show-schedule-interface'
-						},
-						{
-							title: 'Save draft',
-							slug: 'save-draft'
-						},
-						{
-							title: 'Trash',
-							slug: 'trash'
-						},
-						{
-							title: 'Save as pending review',
-							slug: 'save-as-pending-review'
-						}
-					];
-				}
-				if ( this.model.get( 'post_status' ) === 'pending' ) {
-					this.publishingOptions = [
-						{
-							title: 'Update',
-							slug: 'save-as-pending-review'
-						},
-						{
-							title: 'Publish now',
-							slug: 'publish-now'
-						},
-						{
-							title: 'Schedule',
-							slug: 'show-schedule-interface'
-						},
-						{
-							title: 'Publish privately',
-							slug: 'publish-privately'
-						},
-						{
-							title: 'Send back to draft',
-							slug: 'save-draft'
-						},
-						{
-							title: 'Trash',
-							slug: 'trash'
-						}
-					];
-				}
-				$container = $( '<div class="misc-pub-section publish-button clearfix"></div>' );
-				$container.append( $('<button type="button" class="dashicons-before dashicons-search btn publish-action-preview btn-info">Preview</button>' ) );
-
-				$dropdownContainer = $('<div class="dropdown-menu-container btn-group"></div>' );
-				$dropdownUL = $( '<ul class="dropdown-menu" role="menu"></ul> ' );
-
-				_.each( this.publishingOptions, function( element, index, list ) {
-					if ( index === 0 ) {
-						$dropdownContainer.append( $('<button type="button" class="btn btn-primary publish-action-' + element.slug + '">' + element.title + '</button>' ),
-							$( '<button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"><span class="caret"></span><span class="sr-only">Toggle Dropdown</span></button>' ) );
-					} else {
-						$dropdownUL.append( $( '<li><a href="#" class="publish-action-' + element.slug + '">' + element.title + '</a></li>' ) );
-					}
-				});
-
-				$dropdownContainer.append( $dropdownUL );
-				$container.append( $dropdownContainer );
-				$container.insertAfter( '.rad-publish-status' );
-			},
-
-			handleUpdateClick: function() {
-				event.preventDefault();
-				$('#publish').simulate( 'click' );
-			},
-
-			handlePublishPrivatelyClick: function() {
-				event.preventDefault();
-				$('#post_status').val( 'publish' );
-				$('[name="visibility"]').val( 'private' );
-				$('#publish').simulate( 'click' );
-			},
-
-			handlePublishPubliclyClick: function() {
-				event.preventDefault();
-				$('#post_status').val( 'publish' );
-				$('[name="visibility"]').val( 'public' );
-				$('#publish').simulate( 'click' );
-			},
-
-			handlePreviewClick: function( event ) {
-				event.preventDefault();
-				event.stopPropagation();
-				$('#post-preview').simulate( 'click' );
-			},
-
-			handleTrashClick: function( event ) {
-				event.preventDefault();
-				$('#delete-action a').simulate( 'click' );
-			},
-
-			handleSaveDraftClick: function() {
-				event.preventDefault();
-				$('[name="visibility"]').val( 'public' );
-				$('#post_status').val( 'draft' );
-				$('#save-post').simulate( 'click' );
-			},
-
-			handleSaveAsPendingReviewClick: function() {
-				event.preventDefault();
-				$('[name="visibility"]').val( 'public' );
-				$('#post_status').val( 'pending' );
-				if ( $('#save-post').length ) {
-					$('#save-post').simulate( 'click' );
-				} else {
-					$('#publish').simulate( 'click' );
-				}
-			},
-
-			handlePublishNowClick: function() {
-				event.preventDefault();
-				$('#publish').simulate( 'click' );
-			},
-
-			handleShowScheduleInterfaceClick: function() {
-				event.preventDefault();
-				this.$('.schedule').show();
+				$('[name=pmb2-poststatus]').on( 'change', this.handlePostStatusChange );
+				$('.pmb2-publish').on( 'click', _.bind( this.handleMajorPublishActionButtonClick, this ) );
+				$('.pmb2-publish-action-preview').on( 'click', this.handlePreviewClick );
+				$('.pmb2-password').on( 'keyup', this.syncPostPassword );
+				// this.$('.hour-dropdown,.ampm-dropdown,.minute-input').on( 'change keyup', _.bind( this.handleTimeInputsChange, this ) );
 			},
 
 			initializeDatepicker: function() {
-				$('.datepicker').datepicker({
+				$('.pmb2-datepicker').datepicker({
 					onSelect: function( dateString, datepickerInstance ) {
 						var month = datepickerInstance.selectedMonth + 1;
 						if ( month < 10 ) {
 							month = '0' + month;
 						}
 						$('#mm [value="' + month + '"]').prop( 'selected', true );
-
 						$('#jj').val( datepickerInstance.selectedDay );
+						$('#aa').val( datepickerInstance.selectedYear );
 						$('.save-timestamp').simulate('click');
 					}
 				});
 			},
 
-			handleTimeInputsChange: function( event ) {
-				var hours = parseInt( this.$('.hour-dropdown').val() ),
-					ampm = this.$('.ampm-dropdown').val(),
-					minutes = this.$('.minute-input').val();
-				if ( ampm === 'pm' ) {
-					hours += 12;
-				}
-				$('#hh').val( hours );
-				$('#mn').val( minutes );
-				$('.save-timestamp').simulate('click');
+			syncPostPassword: function() {
+				$('[name=post_password]').val( $('.pmb2-password').val() );
 			},
 
-			handleScheduleClick: function() {
-				$('#post_status').val( 'publish' );
-				$('[name="visibility"]').val( 'public' );
-				$('#publish').simulate( 'click' );
+			/**
+			 * When a new post status is selected, update
+			 * the major publishing action button text.
+			 */
+			handlePostStatusChange: function() {
+				var $this = $(this),
+					$relatedLabel = $('[for=' + $this.attr('id') + ']'),
+					newPostStatus = $(this).val();
+
+				$('.pmb2-publish').html( $relatedLabel.html() );
+				switch ( newPostStatus ) {
+					case 'save-as-draft':
+						$('[name="visibility"]').val( 'public' );
+						$('#post_status').val( 'draft' );
+					break;
+					case 'send-to-pending-review':
+						$('[name="visibility"]').val( 'public' );
+						$('#post_status').val( 'pending' );
+					break;
+					case 'publish-now':
+						$('#post_status').val( 'publish' );
+						$('[name="visibility"]').val( 'public' );
+						// @todo
+					break;
+					case 'schedule':
+						$('#post_status').val( 'publish' );
+						$('[name="visibility"]').val( 'public' );
+					break;
+					case 'publish-privately':
+						$('#post_status').val( 'publish' );
+						$('[name="visibility"]').val( 'private' );
+					break;
+					case 'publish-with-password-protection':
+						$('#post_status').val( 'publish' );
+						$('[name="visibility"]').val( 'password' );
+					break;
+					case 'move-to-trash':
+						$('#delete-action a').simulate( 'click' );
+					break;
+				}
+			},
+
+			handleMajorPublishActionButtonClick: function() {
+				var postStatus = $('[name=pmb2-poststatus]').val(),
+					previousStatus = this.model.get('post_status');
+				switch( postStatus ) {
+					case 'publish-now':
+					case 'schedule':
+					case 'publish-privately':
+						$('#publish').simulate( 'click' );
+					break;
+					case 'save-as-draft':
+					case 'send-to-pending-review':
+						$('#save-post').simulate( 'click' );
+					break;
+					case 'update':
+						if ( previousStatus === 'draft' || previousStatus === 'pending' ) {
+							$('#save-post').simulate( 'click' );
+						} else {
+						$('#publish').simulate( 'click' );
+						}
+					break;
+				}
 			}
+
+			// handleTimeInputsChange: function( event ) {
+			// 	var hours = parseInt( this.$('.hour-dropdown').val() ),
+			// 		ampm = this.$('.ampm-dropdown').val(),
+			// 		minutes = this.$('.minute-input').val();
+			// 	if ( ampm === 'pm' ) {
+			// 		hours += 12;
+			// 	}
+			// 	$('#hh').val( hours );
+			// 	$('#mn').val( minutes );
+			// 	$('.save-timestamp').simulate('click');
+			// }
 		});
 
 		new postStatusController();
--- plugin.php
+++ plugin.php
@@ -12,8 +12,6 @@ add_action( 'admin_enqueue_scripts', function() {
 		return;
 	}
 	wp_enqueue_style( 'publish-meta-box-two-point-oh', plugins_url( 'includes/css/plugin.css', __FILE__ ) );
-	// wp_enqueue_style( 'bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css' );
-	wp_enqueue_script( 'bootstrap', plugins_url( 'includes/js/bootstrap.js', __FILE__ ), array( 'jquery' ) );
 	wp_enqueue_style( 'jquery-ui-css', plugins_url( 'includes/jquery-ui/jquery-ui.css', __FILE__ ) );
 
 	wp_register_script( 'jquery-simulate', plugins_url( 'includes/js/jquery.simulate.js', __FILE__ ), array( 'jquery' ) );
@@ -49,32 +47,60 @@ add_action( 'post_submitbox_misc_actions', function() {
 			$message = 'Pending Review';
 		break;
 	}
-	?><div class="misc-pub-rad-publish-container">
-		<div class="misc-pub-section rad-publish-status">Status: <?php echo $message ?></div>
-		<div class="schedule">
-			<div class="misc-pub-section datepicker"></div>
-			<div class="misc-pub-section publish-button"></div>
-			<div class="misc-pub-section time-inputs">
-				<select class="hour-dropdown">
-					<?php for( $i = 0; $i <= 11; $i++ ) :
-						if ( $i == 0 ) {
-							$hour = 12;
-						} else {
-							$hour = $i;
-						}
-
-						?><option value="<?php echo $hour ?>"  <?php selected( get_post_time( 'g', false, $post->ID ), $hour ) ?>><?php echo $hour ?></option>
-					<?php endfor; ?>
-				</select>:<input class="minute-input" maxlength="2" size="2" value="<?php echo get_post_time( 'i', false, $post->ID ) ?>">
-				<select class="ampm-dropdown">
-					<option value="am" <?php selected( get_post_time( 'a', false, $post->ID ), 'am' ) ?>>am</option>
-					<option value="pm" <?php selected( get_post_time( 'a', false, $post->ID ), 'pm' ) ?>>pm</option>
-				</select>
+	?>
+	<div class="pmb2">
+		<div class="misc-pub-section">Current status: <?php echo $message ?></div>
+		<div class="misc-pub-section">
+			<?php if ( $post->post_status === 'auto-draft' ) : ?>
+			<input type="radio" id="pmb2-poststatus-save-as-draft" name="pmb2-poststatus" value="save-as-draft" checked>
+			<label for="pmb2-poststatus-save-as-draft" class="pmb2-poststatus-label">Save as draft</label><br>
+			<?php else : ?>
+			<input type="radio" id="pmb2-poststatus-update" name="pmb2-poststatus" value="update" checked>
+			<label for="pmb2-poststatus-update" class="pmb2-poststatus-label">Update</label><br>
+			<?php if ( $post->post_status !== 'draft' ) : ?>
+			<input type="radio" id="pmb2-poststatus-save-as-draft" name="pmb2-poststatus" value="save-as-draft">
+			<label for="pmb2-poststatus-save-as-draft" class="pmb2-poststatus-label">Send back to draft</label><br>
+			<?php endif; ?>
+			<?php endif; ?>
+			<?php if ( $post->post_status !== 'pending' ) : ?>
+			<input type="radio" id="pmb2-poststatus-send-back-to-pending-review" name="pmb2-poststatus" value="send-to-pending-review">
+			<label for="pmb2-poststatus-send-back-to-pending-review" class="pmb2-poststatus-label">Send to pending review</label><br>
+			<?php endif; ?>
+			<input type="radio" id="pmb2-poststatus-publish" name="pmb2-poststatus" value="publish-now">
+			<label for="pmb2-poststatus-publish" class="pmb2-poststatus-label">Publish now</label><br>
+			<input type="radio" id="pmb2-poststatus-schedule" name="pmb2-poststatus" value="schedule">
+			<label for="pmb2-poststatus-schedule" class="pmb2-poststatus-label">Schedule</label><br>
+			<div class="misc-pub-section">
+				<div class="dashicons dashicons-calendar-alt"></div>
+				<input type="text" class="pmb2-datepicker" value="<?php esc_attr_e( get_the_time( 'm/d/Y' ) ) ?>">
+				<div class="dashicons dashicons-clock"></div>
+				<input type="text" class="pmb2-clock" value="<?php esc_attr_e( get_the_time( 'H:m' ) ) ?>">
 			</div>
-			<div class="misc-pub-section schedule-button">
-				<button type="button" class="button button-primary publish-action-schedule">Schedule</button>
+			<input type="radio" id="pmb2-poststatus-publish-privately" name="pmb2-poststatus" value="publish-privately">
+			<label for="pmb2-poststatus-publish-privately" class="pmb2-poststatus-label">Publish Privately</label><br>
+			<input type="radio" id="pmb2-poststatus-move-to-trash" name="pmb2-poststatus" value="move-to-trash">
+			<label for="pmb2-poststatus-move-to-trash" class="pmb2-poststatus-label">Move to trash</label><br>
+			<div class="misc-pub-section">
+				<input type="text" placeholder="post password" class="pmb2-password" value="<?php esc_attr_e( $post->post_password ) ?>">
 			</div>
 		</div>
-	</div><?php
-
+		<div id="major-publishing-actions">
+			<div id="preview-action">
+				<button type="button" class="button button-large dashicons-before dashicons-search pmb2-publish-action-preview">Preview</button>
+			</div>
+			<div id="publishing-action">
+				<span class="spinner"></span>
+				<input name="original_publish" type="hidden" id="original_publish" value="Publish">
+				<button type="button" name="pmb2-publish" class="pmb2-publish button button-primary button-large">
+					<?php if ( $post->post_status === 'auto-draft' ) : ?>
+					Save as draft
+					<?php else : ?>
+					Update
+					<?php endif; ?>
+				</button>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</div>
+	<?php
 } );
